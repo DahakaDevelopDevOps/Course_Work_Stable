00b17d602ea90b6bdd3122e3e810e5008287114d
Dahaka_Develop <typebooleanfalse@gmail.com>
2024-05-06 17:07:49 +0300

    minor fixes

--- Project/db/models/courses.js
+++ Project/db/models/courses.js
@@ -30,3 +30,10 @@ module.exports = function(sequelize, DataTypes) {
     });
   };
   
+  function formatDate(timeString) {
+    const date = new Date(timeString);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
