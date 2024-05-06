e9edf1ec1cfc27bd6de9ee15bcbf50b2a17681c5
Dahaka_Develop <typebooleanfalse@gmail.com>
2024-05-06 22:56:01 +0300

    course pass added

--- Project/controllers/entryController.js
+++ Project/controllers/entryController.js
@@ -1,5 +1,5 @@
 const { models } = require('../db/utils/db');
-
+const nodemailer = require('nodemailer');
 
 class EntryController {
 
@@ -28,35 +28,89 @@ class EntryController {
         }
     }
 
-    async addEntry(req, res) {
-        try {
-            const courseId = req.params.id;
-            const { email } = req.body; 
-    
-            const existingEntry = await models.Statistics.findOne({
-                where: {
-                    user_id: req.session.userId,
-                    course_id: courseId
-                }
-            });
-            if (existingEntry) {
-                res.status(400).send('Вы уже записаны на этот курс');
+async addEntry(req, res) {
+    try {
+        const courseId = req.params.id;
+        const { email } = req.body; 
+
+        const existingEntry = await models.Statistics.findOne({
+            where: {
+                user_id: req.session.userId,
+                course_id: courseId
             }
-    
-            const newEntry = await models.Statistics.create({
-                user_id: req.session.userId, 
-                course_id: courseId,
-                start_date: new Date(), 
-                status_id: 2, 
-            });
-    
-            res.redirect('/courses');
-        } catch (error) {
-            // В случае ошибки возвращаем статус 500 и сообщение об ошибке
-            console.error('Ошибка при записи на курс:', error);
-            res.status(500).send('Произошла ошибка при записи на курс');
+        });
+        if (existingEntry) {
+            return res.status(400).send('Вы уже записаны на этот курс');
         }
+
+        const newEntry = await models.Statistics.create({
+            user_id: req.session.userId, 
+            course_id: courseId,
+            start_date: new Date(), 
+            status_id: 2, 
+        });
+
+        // Отправляем сообщение пользователю о записи на курс
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'typebooleanfalse@gmail.com',
+                pass: 'jkds firs ahzr ardn'
+            }
+        });
+
+        const mailOptions = {
+            from: 'courseproject@gmail.com',
+            to: email, // Используем email, указанный пользователем при записи
+            subject: 'Вы успешно записались на курс',
+            text: 'Спасибо за ваш выбор! Вы успешно записались на курс. Пройти его можно по ссылке: https://drive.google.com/file/d/1uCOfNp2l2hs1IMO5f9TNzOOVn7hoAIc4/view?usp=sharing'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.error('Ошибка при отправке сообщения:', error);
+            } else {
+                console.log('Сообщение успешно отправлено:', info.response);
+            }
+        });
+
+        res.redirect('/courses');
+    } catch (error) {
+        console.error('Ошибка при записи на курс:', error);
+        res.status(500).send('Произошла ошибка при записи на курс');
     }
+}
+
+
+    // async addEntry(req, res) {
+    //     try {
+    //         const courseId = req.params.id;
+    //         const { email } = req.body; 
+    
+    //         const existingEntry = await models.Statistics.findOne({
+    //             where: {
+    //                 user_id: req.session.userId,
+    //                 course_id: courseId
+    //             }
+    //         });
+    //         if (existingEntry) {
+    //             res.status(400).send('Вы уже записаны на этот курс');
+    //         }
+    
+    //         const newEntry = await models.Statistics.create({
+    //             user_id: req.session.userId, 
+    //             course_id: courseId,
+    //             start_date: new Date(), 
+    //             status_id: 2, 
+    //         });
+    
+    //         res.redirect('/courses');
+    //     } catch (error) {
+    //         // В случае ошибки возвращаем статус 500 и сообщение об ошибке
+    //         console.error('Ошибка при записи на курс:', error);
+    //         res.status(500).send('Произошла ошибка при записи на курс');
+    //     }
+    // }
     
 
 }
