5f94258f5c023f2795b17b2388dba9a516d775d0
Dahaka_Develop <typebooleanfalse@gmail.com>
2024-05-09 11:59:59 +0300

    many changes, validations, marks

--- Project/controllers/homeController.js
+++ Project/controllers/homeController.js
@@ -1,5 +1,4 @@
 // const asyncHandler = require("express-async-handler");
-//ето можно добавить, чтобы не спамить по кд try/catch
 
 exports.getMainPage = (req, res, next) => {
     res.render("./layouts/home.hbs", { layout: "home.hbs" });
